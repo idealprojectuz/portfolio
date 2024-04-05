176f7ba86586a3b58d094ed5662a04cac89cfce4
Hayotbek <idealprojectuz@gmail.com>
2024-04-05 21:41:01 +0500

    first

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+  email: "hayotbeksamandarov0427@gmail.com",
+  phone: "+998900860011",
+  address: "Navoiy region khatirchi district",
+  github: "https://github.com/idealprojectuz",
+  facebook: "https://www.facebook.com/excellendboy/",
+  linkedIn: "https://www.linkedin.com/in/excellendboy/",
+  twitter: "https://x.com/excellend_boy",
+  stackOverflow: "https://stackoverflow.com/users/16965165/hayotbek-samandarov",
+  devUsername: "idealprojectuz",
+};
---
+++ utils/data/personal-data.js
@@ -0,0 +1,18 @@
+export const personalData = {
+  name: "Hayotbek",
+  profile: "/profile-me.jpg",
+  designation: "Full stack developer",
+  description:
+    "My name is Hayotbek. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: "hayotbeksamandarov0427@gmail.com",
+  phone: "+998900860011",
+  address: "Navoiy region khatirchi district",
+  github: "https://github.com/idealprojectuz",
+  facebook: "https://www.facebook.com/excellendboy/",
+  linkedIn: "https://www.linkedin.com/in/excellendboy/",
+  twitter: "https://x.com/excellend_boy",
+  stackOverflow: "https://stackoverflow.com/users/16965165/hayotbek-samandarov",
+  leetcode: "https://leetcode.com/idealprojectuz/",
+  devUsername: "idealprojectuz",
+  resume: "#",
+};
